3c74353b9510fb52f1c47975b96d26ae03ed9dae
Abdul Moeed Saqib <59030881+Abdul-Moeed-Saqib@users.noreply.github.com>
2024-11-04 17:29:06 -0500

    Updated scraping

--- backend/scrapers/specs_scraper.py
+++ backend/scrapers/specs_scraper.py
@@ -185,20 +185,12 @@ def extract_features(part_name):
 
 # This function searches prices of the parts using web scraping
 def search_part_price(part_name, component_type=None):
+    scraper_api_key = os.getenv('SCRAPERAPI_KEY') 
     search_url = f"https://www.newegg.com/p/pl?d={component_type.replace(' ', '+')}"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Referer': 'https://www.newegg.com/',
-        'DNT': '1',  
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
-    }
-    
+    api_url = f"http://api.scraperapi.com/?api_key={scraper_api_key}&url={search_url}"
+
     try:
-        time.sleep(1)
-        response = requests.get(search_url, headers=headers)
+        response = requests.get(api_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
             part_features = extract_features(component_type)  
