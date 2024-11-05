03ece9fd344e58795586ae468cb4771cd6976ba5
Abdul Moeed Saqib <59030881+Abdul-Moeed-Saqib@users.noreply.github.com>
2024-11-05 16:35:20 -0500

    Updated env function

--- backend/app.py
+++ backend/app.py
@@ -65,4 +65,4 @@ def scrape():
     return response, 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
