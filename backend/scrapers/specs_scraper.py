4c38fc7840ac9730d5e05798dcb2d90ee4c333a7
Abdul Moeed Saqib <59030881+Abdul-Moeed-Saqib@users.noreply.github.com>
2024-11-05 01:31:03 -0500

    updated error handling

--- backend/scrapers/specs_scraper.py
+++ backend/scrapers/specs_scraper.py
@@ -17,7 +17,7 @@ def get_html_content(url):
     try:
         response = requests.get(url)
         if response.status_code != 200:
-            return None, f"Failed to fetch page content. Status code: {response.status_code}"
+            return None, f"Failed to fetch page content. Status code: {response.text}"
         
         html_content = response.content
     except requests.exceptions.RequestException as e:
