66d94838a636a29d14cd7f2413264f520949fd4c
Abdul Moeed Saqib <59030881+Abdul-Moeed-Saqib@users.noreply.github.com>
2024-11-11 00:56:56 -0500

    Fixed api request in server

--- backend/app.py
+++ backend/app.py
@@ -73,6 +73,7 @@ def scrape():
 @app.route('/api/health', methods=['GET'])
 def health_check():
     print('Health ok')
+    return None
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
